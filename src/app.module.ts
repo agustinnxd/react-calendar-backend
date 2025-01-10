afcd05342dd0c8b8f4336d375b7bc0640f7a33c3
Agustin <agustinnxd>
2025-01-10 16:47:19 -0300

    React App deployed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventsModule } from './events/events.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 require('dotenv').config();
 
 @Module({
-  imports: [AuthModule,
-    MongooseModule.forRoot(process.env.MONGO_CNN), EventsModule
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_CNN), EventsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    })
   ],
   controllers: [],
   providers: [],
